3904533b0d1f6f410482c3da22bd860369ccbcec
julien382 <julhermain@gmail.com>
2023-03-22 01:31:35 +0100

    link logo profile

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -26,10 +26,10 @@ const Header = () => {
                 <div>
                     {isAuthenticated ? (
                         <div className='row'>
-                            <div className='row'>
+                            <Link to="/user"><div className='row'>
                                 <img src={iconProfile} alt="Profile Icon"/>
                                 <p className='firstName'>{firstName}</p>
-                            </div>
+                            </div></Link>
                             <Link onClick={handleLogout} className="main-nav-item" to="/">
                                 <i className="fa fa-sign-out"></i>Sign Out
                             </Link>
